ba903791056a023f65dfae6db269362df3e23bda
Ben Cooley <benjamincooley94@gmail.com>
2020-04-14 09:59:39 -0400

    new Vue page for case study template

--- pages/spotify-data-design.vue
+++ pages/spotify-data-design.vue
@@ -9,41 +9,62 @@
 
     <b-container class="project-intro"
     fluid>
-        <h2>Music labels and artists want to know the value of Spotify. The insights team at Spotify needed new visual methods to discover, share and communicate findings from streaming data.</h2>
+        <h3>Music labels and artists want to know the value of Spotify. The insights team at Spotify needed new visual methods to discover, share and communicate findings from streaming data.</h3>
     </b-container>
 
+    <!-- description of project -->
     <b-container fluid>
-        <b-row no-gutters>
-            <b-col md>
-                <b-container>
-                    <h2>The Project</h2>
-                    <p>
-                        The insights team at Spotify London had questions: what is the point of maximum ROI for programmed streams per song? How much should music managers invest before putting on the brakes? When are the most important boost times?
-                    </p>
-                    <p>
-                        To answer these questions, Spotify used their analytic models to investigate. But they kept running into a problem: how do we effectively communicate our findings in a way that both informs and excites people?
-                    </p>
-                    <p>
-                        In addition, Spotify was interested in using visualization as a way to not only answer questions, but to <strong style="font-weight: 600">ask new questions in the process</strong>.
-                    </p>
-                </b-container>
-            </b-col>
-            <b-col md>
-                <b-img fluid src="/images/projects_spotify.png"></b-img>
-            </b-col>
+        <b-row>
+          <b-col md="1"></b-col>
+          <b-col cols="12" md="7">
+            <transition name="slide-fade">
+              <b-container v-if="loaded" class="project-description">
+                  <h2>The Project</h2>
+                  <p>
+                      The insights team at Spotify London had questions: what is the point of maximum ROI for programmed streams per song? How much should music managers invest before putting on the brakes? When are the most important boost times?
+                  </p>
+                  <p>
+                      To answer these questions, Spotify used their analytic models to investigate. But they kept running into a problem: how do we effectively communicate our findings in a way that both informs and excites people?
+                  </p>
+                  <p>
+                      In addition, Spotify was interested in using visualization as a way to not only answer questions, but to <strong style="font-weight: 600">ask new questions in the process</strong>.
+                  </p>
+              </b-container>
+            </transition>
+          </b-col>
+          <b-col md="4"></b-col>
         </b-row>
     </b-container>
 
+    <!-- insert some photos or grid of photos -->
     <b-container fluid>
         <b-row no-gutters>
-            <b-col order-md="1" md>
-                <b-container>
-                    <h2>The Solution</h2>
-                </b-container>
-            </b-col>
-            <b-col md>
+            <b-col md="2"></b-col>
+            <b-col md="8">
                 <b-img fluid src="/images/projects_spotify.png"></b-img>
             </b-col>
+            <b-col md="2"></b-col>
+        </b-row>
+    </b-container>
+
+    <!-- contributions / detail of solution -->
+    <b-container fluid>
+        <b-row>
+          <b-col md="4"></b-col>
+          <b-col cols="12" md="7">
+            <transition name="slide-fade">
+              <b-container v-if="loaded" class="project-description">
+                  <h2>Contributions</h2>
+                  <p>
+                      To start, I analyzed an anonymized dataset provided by Spotify. This data contained variables like date of song release, number of streams, artist, genre, and source of streaming. From this analysis, I created the visual metaphor of the shape of a song.
+                  </p>
+                  <p>
+                      I then exported each song triangle and worked with a team of designers to build different concepts around each. We wanted to paint a picture for Spotify of what their future analytics platform could be. Because Spotify works with music managers, labels, and often the artists themselves, we wanted the methods of visualization to be both informative and exciting in a way that matched Spotify's brand.
+                  </p>
+              </b-container>
+            </transition>
+          </b-col>
+          <b-col md="1"></b-col>
         </b-row>
     </b-container>
 
@@ -60,6 +81,9 @@
       loaded: false
     }
   },
+  mounted() {
+    this.loaded = true;
+  },
   methods: {
     onLoaded() {
       this.loaded = true;
@@ -73,54 +97,64 @@
     margin: auto
   }
 
-  .jumbotron h1 {
-      font-family: "CircMed", sans-serif
+  .box{
+    position: relative;
+    display: inline-block; /* Make the width of box same as image */
+  }
+  
+  .box .header-text{
+      position: absolute;
+      z-index: 999;
+      margin: 0 auto;
+      left: 0;
+      right: 0;
+      color: white;
+      top: 10%; /* Adjust this value to move the positioned div up and down */
+      text-align: center;
+      width: 60%; /* Set the width of the positioned div */
     }
 
-  .about-image {
-    margin-bottom: 30px
-  }
-
-  .about-text {
-    padding: 10px
-  }
-
-  .about-text p {
-    padding: 10px;
-    font-size: 21px
-  }
-
-      .box{
-        position: relative;
-        display: inline-block; /* Make the width of box same as image */
-    }
-    .box .header-text{
-        position: absolute;
-        z-index: 999;
-        margin: 0 auto;
-        left: 0;
-        right: 0;
-        color: white;
-        top: 10%; /* Adjust this value to move the positioned div up and down */
-        text-align: center;
-        width: 60%; /* Set the width of the positioned div */
-    }
-
-    .header-text h1 {
+    h1 {
         font-size: 5rem;
+    }
+
+    h2 {
+      font-size: 3rem
+    }
+
+    h3 {
+      font-size: 2rem
     }
 
     .project-intro {
         height: 300px;
         text-align: center;
         margin-top: 100px;
-        top: 30%
+        top: 30%;
+        max-width: 700px
+    }
+
+    .project-description {
+      margin-bottom: 300px;
+      margin-top: 150px
     }
 
     p {
         font-size: 21px;
-        max-width: 550px;
-        
+    }
+
+    /* Enter and leave animations can use different */
+    /* durations and timing functions.              */
+    .slide-fade-enter-active {
+      transition: all .6s ease;
+    }
+    .slide-fade-leave-active {
+      transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    }
+    .slide-fade-enter, .slide-fade-leave-to
+    /* .slide-fade-leave-active below version 2.1.8 */ {
+      transform: translateX(10px);
+      opacity: 0;
     }
 
 </style>
