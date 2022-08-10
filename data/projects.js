6358c059077928021f4b4d0cee9d8b161ae84751
Ben Cooley <benjamincooley94@gmail.com>
2022-08-10 16:43:53 -0400

    store single page portfolio layout

---
+++ data/projects.js
@@ -0,0 +1,34 @@
+const data = [
+    {
+        title: "Webster UMAP",
+        description:
+            "A 3d visualization tool that shows the correlation of certain biological functions to sets of genes.",
+        type: "data visualization",
+        imgUrl: "../images/projects/webster.png",
+        url: "https://broadinstitute.github.io/webster-umap-minisite/#/",
+    },
+  {
+      title: "Global Flood Dashboard",
+      description:
+          "Design for one of the world’s largest collection of flood maps.",
+      type: "data visualization",
+      imgUrl: "../images/front-page/c2s-dashboard.png",
+      url: "https://global-flood-database.cloudtostreet.info/",
+  },
+  {
+      title: "To All the Books I Read in 2020",
+      description:
+          "Data viz essay looking back on the books I read in 2020 // 01/2021",
+      type: "data visualization",
+      imgUrl: "../images/front-page/book-scrolly.png",
+      url: "https://objective-wiles-09a6fb.netlify.app/",
+  },
+  {
+      title: "2019 - a year of data visualized",
+      description:
+          "A visual retrospective of the data I tracked during 2019.",
+      type: "data visualization",
+      imgUrl: "../images/front-page/workouts.png",
+      url: "https://benjamincooley.com/experimental-projects/2019-review/",
+  },
+];
