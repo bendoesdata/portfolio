640d7a6782c06151d10534126ebab72586a5ee0d
bendoesdata <benjamincooley94@gmail.com>
2023-01-02 14:19:36 -0500

    style tweaks

--- data/projects.js
+++ data/projects.js
@@ -2,7 +2,7 @@ const data = [
     {
         title: "3d Webster UMAP",
         description:
-            "A 3d visualization tool that shows the correlation of certain biological functions to sets of genes.",
+            "A 3d visualization tool that maps biological funtions to genes.",
         type: "data visualization",
         imgUrl: "../images/projects/webster.png",
         url: "https://broadinstitute.github.io/webster-umap-minisite/#/",
@@ -32,15 +32,15 @@ const data = [
     url: "https://parametric.press/issue-02/algae/",
     },
   {
-      title: "To All the Books I Read in 2020",
+      title: "To All the Books in 2020",
       description:
-          "Data viz essay looking back on the books I read in 2020 // 01/2021",
+          "Data viz essay looking back on the books I read in 2020.",
       type: "data visualization",
       imgUrl: "../images/front-page/book-scrolly.png",
       url: "https://objective-wiles-09a6fb.netlify.app/",
   },
   {
-      title: "2019 - a year of data visualized",
+      title: "2019 - Visualized",
       description:
           "A visual retrospective of the data I tracked during 2019.",
       type: "data visualization",
